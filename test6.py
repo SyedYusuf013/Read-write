2b1c1b7a4e432494f98d0e18aae5bf99a1c2ea30
Syed Fayeque Uddin Yusuf <130254164+SyedYusuf013@users.noreply.github.com>
2023-08-06 18:24:49 +0530

    Add files via upload

---
+++ test6.py
@@ -0,0 +1,12 @@
+data = {
+    "name":"syed",
+    "course": ["ml", "dl", "py" , "stats" , "cv" ],
+    "msg":"this is my class "
+}
+
+def get_course():
+    data["course"]
+    
+    
+def msg():
+    return data["msg"]
